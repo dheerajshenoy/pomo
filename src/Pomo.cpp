393ff920b5d099ce3e768479f385097b06ea9742
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-05-30 14:55:01 +0530

    Add ability to go next task and next session

--- src/Pomo.cpp
+++ src/Pomo.cpp
@@ -1,13 +1,14 @@
 #include "Pomo.hpp"
+
 #include <qnamespace.h>
 #include <qt6/QtWidgets/qsystemtrayicon.h>
 
 Pomo::Pomo()
 {
-    m_timer_font = m_timer_label->font();
-    m_state_font = m_state_label->font();
+    m_timer_font     = m_timer_label->font();
+    m_state_font     = m_state_label->font();
     m_remaining_font = m_remaining_label->font();
-    m_paused_font = m_paused_label->font();
+    m_paused_font    = m_paused_label->font();
 
     initActionMap();
     initConfiguration();
@@ -20,19 +21,36 @@ Pomo::Pomo()
     m_timer.setTimerType(Qt::TimerType::PreciseTimer);
 }
 
-void Pomo::initActionMap() noexcept
+void
+Pomo::initActionMap() noexcept
 {
-    m_action_map["reset"] = [this]() { resetTimer(); };
-    m_action_map["toggle"] = [this]() { toggleTimer(); };
+    m_action_map["reset"] = [this]()
+    {
+        resetTimer();
+    };
+    m_action_map["toggle"] = [this]()
+    {
+        toggleTimer();
+    };
+    m_action_map["next-task"] = [this]()
+    {
+        advanceState();
+    };
+
+    m_action_map["next-session"] = [this]()
+    {
+        nextSession();
+    };
 }
 
-void Pomo::initConfiguration() noexcept
+void
+Pomo::initConfiguration() noexcept
 {
-    QString xdg_config = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
+    QString xdg_config       = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
     QString config_file_path = QDir(xdg_config).filePath("pomo/config.toml");
     QFile config_file(config_file_path);
 
-    if(!config_file.exists())
+    if (!config_file.exists())
     {
         return;
     }
@@ -41,9 +59,9 @@ void Pomo::initConfiguration() noexcept
 
     auto font_name = config["time"]["font"].value_or("Noto Sans");
     auto font_size = config["time"]["font-size"].value_or(80);
-    auto bold = config["time"]["bold"].value_or(true);
-    auto italic = config["time"]["italic"].value_or(false);
-    auto color = config["time"]["color"].value<std::string>();
+    auto bold      = config["time"]["bold"].value_or(true);
+    auto italic    = config["time"]["italic"].value_or(false);
+    auto color     = config["time"]["color"].value<std::string>();
     m_timer_font.setFamily(QString::fromStdString(font_name));
     m_timer_font.setPixelSize(font_size);
     m_timer_font.setBold(bold);
@@ -51,17 +69,16 @@ void Pomo::initConfiguration() noexcept
     if (color.has_value())
     {
         auto palette = m_timer_label->palette();
-        palette.setColor(QPalette::WindowText,
-                         QString::fromStdString(color.value()));
+        palette.setColor(QPalette::WindowText, QString::fromStdString(color.value()));
         m_timer_label->setPalette(palette);
     }
 
-    m_state_shown = config["state"]["shown"].value_or(true);
+    m_state_shown        = config["state"]["shown"].value_or(true);
     auto state_font_name = config["state"]["font"].value_or("Noto Sans");
     auto state_font_size = config["state"]["font-size"].value_or(80);
-    auto state_bold = config["state"]["bold"].value_or(true);
-    auto state_italic = config["state"]["italic"].value_or(false);
-    auto state_color = config["state"]["color"].value<std::string>();
+    auto state_bold      = config["state"]["bold"].value_or(true);
+    auto state_italic    = config["state"]["italic"].value_or(false);
+    auto state_color     = config["state"]["color"].value<std::string>();
     m_state_font.setFamily(QString::fromStdString(state_font_name));
     m_state_font.setPixelSize(state_font_size);
     m_state_font.setBold(state_bold);
@@ -70,16 +87,15 @@ void Pomo::initConfiguration() noexcept
     if (state_color.has_value())
     {
         auto state_palette = m_timer_label->palette();
-        state_palette.setColor(QPalette::WindowText,
-                               QString::fromStdString(state_color.value()));
+        state_palette.setColor(QPalette::WindowText, QString::fromStdString(state_color.value()));
         m_timer_label->setPalette(state_palette);
     }
 
     auto remaining_font_name = config["remaining"]["font"].value_or("Noto Sans");
     auto remaining_font_size = config["remaining"]["font-size"].value_or(80);
-    auto remaining_bold = config["remaining"]["bold"].value_or(true);
-    auto remaining_italic = config["remaining"]["italic"].value_or(false);
-    auto remaining_color = config["remaining"]["color"].value<std::string>();
+    auto remaining_bold      = config["remaining"]["bold"].value_or(true);
+    auto remaining_italic    = config["remaining"]["italic"].value_or(false);
+    auto remaining_color     = config["remaining"]["color"].value<std::string>();
     m_remaining_font.setFamily(QString::fromStdString(remaining_font_name));
     m_remaining_font.setPixelSize(remaining_font_size);
     m_remaining_font.setBold(remaining_bold);
@@ -88,16 +104,15 @@ void Pomo::initConfiguration() noexcept
     if (remaining_color.has_value())
     {
         auto remaining_palette = m_timer_label->palette();
-        remaining_palette.setColor(QPalette::WindowText,
-                               QString::fromStdString(remaining_color.value()));
+        remaining_palette.setColor(QPalette::WindowText, QString::fromStdString(remaining_color.value()));
         m_timer_label->setPalette(remaining_palette);
     }
 
     auto paused_font_name = config["paused"]["font"].value_or("Noto Sans");
     auto paused_font_size = config["paused"]["font-size"].value_or(80);
-    auto paused_bold = config["paused"]["bold"].value_or(true);
-    auto paused_italic = config["paused"]["italic"].value_or(false);
-    auto paused_color = config["paused"]["color"].value<std::string>();
+    auto paused_bold      = config["paused"]["bold"].value_or(true);
+    auto paused_italic    = config["paused"]["italic"].value_or(false);
+    auto paused_color     = config["paused"]["color"].value<std::string>();
     m_paused_font.setFamily(QString::fromStdString(paused_font_name));
     m_paused_font.setPixelSize(paused_font_size);
     m_paused_font.setBold(paused_bold);
@@ -106,8 +121,7 @@ void Pomo::initConfiguration() noexcept
     if (paused_color.has_value())
     {
         auto paused_palette = m_paused_label->palette();
-        paused_palette.setColor(QPalette::WindowText,
-                               QString::fromStdString(paused_color.value()));
+        paused_palette.setColor(QPalette::WindowText, QString::fromStdString(paused_color.value()));
         m_paused_label->setPalette(paused_palette);
     }
     m_paused_shown = config["paused"]["shown"].value_or(true);
@@ -116,16 +130,16 @@ void Pomo::initConfiguration() noexcept
 
     m_hide_hour = config["general"]["hide-hour"].value_or(true);
 
-    auto focus_time = config["pomodoro"]["focus"].value_or("25m");
+    auto focus_time       = config["pomodoro"]["focus"].value_or("25m");
     auto short_break_time = config["pomodoro"]["short-break"].value_or("5m");
-    auto long_break_time = config["pomodoro"]["long-break"].value_or("15m");
+    auto long_break_time  = config["pomodoro"]["long-break"].value_or("15m");
 
-    m_state_time_map[PomodoroState::FOCUS] = parse_duration(focus_time);
+    m_state_time_map[PomodoroState::FOCUS]       = parse_duration(focus_time);
     m_state_time_map[PomodoroState::SHORT_BREAK] = parse_duration(short_break_time);
-    m_state_time_map[PomodoroState::LONG_BREAK] = parse_duration(long_break_time);
+    m_state_time_map[PomodoroState::LONG_BREAK]  = parse_duration(long_break_time);
 
     m_show_notif = config["pomodoro"]["notification"].value_or(true);
-    auto cmd = config["pomodoro"]["notify-command"].value<std::string>();
+    auto cmd     = config["pomodoro"]["notify-command"].value<std::string>();
 
     if (cmd.has_value())
     {
@@ -151,23 +165,25 @@ void Pomo::initConfiguration() noexcept
     {
         auto keybindings = config["keybindings"].as_table();
 
-        for (auto& [k, v] : *keybindings)
+        for (auto &[k, v] : *keybindings)
         {
             auto action = QString::fromStdString(k.str().data());
-            auto key = QString::fromStdString(v.value<std::string>()->data());
+            auto key    = QString::fromStdString(v.value<std::string>()->data());
             setupKeybindings(key, action);
         }
-    } else {
+    }
+    else
+    {
         // Default keybindings
     }
 }
 
-void Pomo::initGui() noexcept
+void
+Pomo::initGui() noexcept
 {
     QWidget *widget = new QWidget();
     widget->setLayout(m_layout);
     this->setCentralWidget(widget);
-
 
     if (m_paused_shown)
     {
@@ -176,7 +192,6 @@ void Pomo::initGui() noexcept
     m_layout->addStretch(0);
 
     m_layout->addWidget(m_timer_label);
-
     m_paused_label->setVisible(false);
 
     if (m_state_shown)
@@ -208,16 +223,17 @@ void Pomo::initGui() noexcept
         notif->show();
         m_layout->addWidget(notif);
     }
-
 }
 
-void Pomo::resetTimer() noexcept
+void
+Pomo::resetTimer() noexcept
 {
     m_timer.stop();
     m_pomodoro_count = 0;
 }
 
-void Pomo::toggleTimer() noexcept
+void
+Pomo::toggleTimer() noexcept
 {
     m_timer_is_active = !m_timer_is_active;
 
@@ -227,21 +243,24 @@ void Pomo::toggleTimer() noexcept
         m_timer.start();
         updateStateLabel();
         m_paused_label->stopBlinking();
-    } else {
+    }
+    else
+    {
         m_timer.stop();
         m_paused_label->startBlinking();
     }
     m_paused_label->setVisible(!m_timer_is_active);
 }
 
-void Pomo::updateCountdown() noexcept
+void
+Pomo::updateCountdown() noexcept
 {
     if (m_totalSeconds > 0)
     {
         int total = --m_totalSeconds;
-        int h = total / 3600;
-        int m = (total % 3600) / 60;
-        int s = total % 60;
+        int h     = total / 3600;
+        int m     = (total % 3600) / 60;
+        int s     = total % 60;
 
         QString timeStr = QString::asprintf("%02d:%02d:%02d", h, m, s);
 
@@ -251,21 +270,23 @@ void Pomo::updateCountdown() noexcept
         }
 
         m_timer_label->setText(timeStr);
-    } else {
+    }
+    else
+    {
         playSound();
         advanceState();
         showNotification();
     }
-
 }
 
-void Pomo::setupKeybindings(const QString &key,
-                            const QString &action) noexcept
+void
+Pomo::setupKeybindings(const QString &key, const QString &action) noexcept
 {
 
     QShortcut *shortcut = new QShortcut(QKeySequence(key), this);
 
-    connect(shortcut, &QShortcut::activated, [this, action]() {
+    connect(shortcut, &QShortcut::activated, [this, action]()
+    {
         if (m_action_map.find(action.toStdString()) != m_action_map.end())
         {
             m_action_map[action.toStdString()]();
@@ -274,72 +295,85 @@ void Pomo::setupKeybindings(const QString &key,
 }
 
 // Initialize audio engine
-void Pomo::initAudioEngine() noexcept
+void
+Pomo::initAudioEngine() noexcept
 {
     if (!m_has_audio)
         return;
 
     m_audio_result = ma_engine_init(NULL, &m_audio_engine);
-    if (m_audio_result != MA_SUCCESS) {
+    if (m_audio_result != MA_SUCCESS)
+    {
         qCritical("Failed to initialize engine\n");
         return;
     }
-
 }
 
 // Play audio file
-void Pomo::playSound() noexcept
+void
+Pomo::playSound() noexcept
 {
     if (!m_has_audio)
         return;
 
     m_audio_result = ma_engine_play_sound(&m_audio_engine, m_audio_file.c_str(), NULL);
-    if (m_audio_result != MA_SUCCESS) {
+    if (m_audio_result != MA_SUCCESS)
+    {
         qWarning("Failed to play sound\n");
         return;
     }
 }
 
-void Pomo::showNotification() noexcept
+void
+Pomo::showNotification() noexcept
 {
     if (!m_show_notif)
         return;
 
-    auto str = replacePlaceholder(m_notify_cmd, "{state}",
-                                  m_state_str_map[m_current_state]);
+    auto str = replacePlaceholder(m_notify_cmd, "{state}", m_state_str_map[m_current_state]);
 
     system(str.c_str());
 }
 
-std::string Pomo::replacePlaceholder(std::string input,
-                                     const std::string& key,
-                                     const std::string& value) noexcept
+std::string
+Pomo::replacePlaceholder(std::string input, const std::string &key, const std::string &value) noexcept
 {
     size_t pos = input.find(key);
     while (pos != std::string::npos)
     {
         input.replace(pos, key.length(), value);
-        pos = input.find(key, pos + value.length()); // Continue search after replacement
+        pos = input.find(key,
+                         pos + value.length()); // Continue search after replacement
     }
     return input;
 }
 
-int Pomo::parse_duration(const std::string& input) noexcept
+int
+Pomo::parse_duration(const std::string &input) noexcept
 {
     std::regex re(R"((\d+)([hms]))");
     std::sregex_iterator it(input.begin(), input.end(), re), end;
 
     int total_seconds = 0;
 
-    while (it != end) {
+    while (it != end)
+    {
         int value = std::stoi((*it)[1].str());
         char unit = (*it)[2].str()[0];
 
-        switch (unit) {
-            case 'h': total_seconds += value * 3600; break;
-            case 'm': total_seconds += value * 60; break;
-            case 's': total_seconds += value; break;
-            default: break; // skip unknown units
+        switch (unit)
+        {
+            case 'h':
+                total_seconds += value * 3600;
+                break;
+            case 'm':
+                total_seconds += value * 60;
+                break;
+            case 's':
+                total_seconds += value;
+                break;
+            default:
+                break; // skip unknown units
         }
         ++it;
     }
@@ -347,18 +381,20 @@ int Pomo::parse_duration(const std::string& input) noexcept
     return total_seconds;
 }
 
-void Pomo::initPomodoro() noexcept
+void
+Pomo::initPomodoro() noexcept
 {
     m_totalSeconds = m_state_time_map[m_current_state];
-    auto str_secs = secondsToFlexibleString(m_totalSeconds);
+    auto str_secs  = secondsToFlexibleString(m_totalSeconds);
     m_timer_label->setText(QString::fromStdString(str_secs));
 
-    m_state_str_map[PomodoroState::FOCUS] = "Focus";
+    m_state_str_map[PomodoroState::FOCUS]       = "Focus";
     m_state_str_map[PomodoroState::SHORT_BREAK] = "Short Break";
-    m_state_str_map[PomodoroState::LONG_BREAK] = "Long Break";
+    m_state_str_map[PomodoroState::LONG_BREAK]  = "Long Break";
 }
 
-std::string Pomo::secondsToFlexibleString(const int &totalSeconds) noexcept
+std::string
+Pomo::secondsToFlexibleString(const int &totalSeconds) noexcept
 {
     int hours   = totalSeconds / 3600;
     int minutes = (totalSeconds % 3600) / 60;
@@ -366,17 +402,18 @@ std::string Pomo::secondsToFlexibleString(const int &totalSeconds) noexcept
 
     std::ostringstream oss;
 
-    if (hours > 0) {
+    if (hours > 0)
+    {
         oss << std::setw(2) << std::setfill('0') << hours << ":";
     }
 
-    oss << std::setw(2) << std::setfill('0') << minutes << ":"
-        << std::setw(2) << std::setfill('0') << seconds;
+    oss << std::setw(2) << std::setfill('0') << minutes << ":" << std::setw(2) << std::setfill('0') << seconds;
 
     return oss.str();
 }
 
-void Pomo::closeEvent(QCloseEvent *e)
+void
+Pomo::closeEvent(QCloseEvent *e)
 {
     if (!m_confirm_on_exit)
     {
@@ -392,9 +429,10 @@ void Pomo::closeEvent(QCloseEvent *e)
         e->ignore();
 }
 
-void Pomo::advanceState() noexcept
+void
+Pomo::advanceState() noexcept
 {
-    switch(m_current_state)
+    switch (m_current_state)
     {
         case PomodoroState::FOCUS:
             m_pomodoro_count++;
@@ -418,7 +456,8 @@ void Pomo::advanceState() noexcept
     updateRemainingLabel();
 }
 
-void Pomo::updateStateLabel() noexcept
+void
+Pomo::updateStateLabel() noexcept
 {
     switch (m_current_state)
     {
@@ -434,10 +473,11 @@ void Pomo::updateStateLabel() noexcept
     }
 }
 
-void Pomo::updateRemainingLabel() noexcept
+void
+Pomo::updateRemainingLabel() noexcept
 {
-    m_remaining_label->setText(QString("%1 tasks before long break")
-                               .arg(m_pomodoros_before_long_break - m_pomodoro_count));
+    m_remaining_label->setText(
+        QString("%1 tasks before long break").arg(m_pomodoros_before_long_break - m_pomodoro_count));
 }
 
 Pomo::~Pomo()
@@ -448,3 +488,11 @@ Pomo::~Pomo()
     }
 }
 
+void
+Pomo::nextSession() noexcept
+{
+    m_current_state               = PomodoroState::LONG_BREAK;
+    m_pomodoros_before_long_break = 4;
+    m_pomodoro_count = 0;
+    advanceState();
+}
--- src/Pomo.hpp
+++ src/Pomo.hpp
@@ -1,27 +1,28 @@
 #pragma once
 
+#include "BlinkingLabel.hpp"
+#include "Notification.hpp"
+#include "miniaudio.h"
+#include "toml.hpp"
+
 #include <QApplication>
-#include <QMainWindow>
-#include <QLabel>
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QTimer>
-#include <QTime>
-#include <QShortcut>
-#include <QKeySequence>
-#include <QDir>
-#include <QStandardPaths>
 #include <QCloseEvent>
-#include <QMessageBox>
+#include <QDir>
 #include <QFile>
+#include <QKeySequence>
+#include <QLabel>
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QShortcut>
+#include <QStandardPaths>
+#include <QTime>
+#include <QTimer>
+#include <QVBoxLayout>
+#include <QWidget>
 #include <map>
 #include <regex>
+#include <type_traits>
 #include <unordered_map>
-
-#include "BlinkingLabel.hpp"
-#include "toml.hpp"
-#include "miniaudio.h"
-#include "Notification.hpp"
 
 class Pomo : public QMainWindow
 {
@@ -37,47 +38,41 @@ private:
     {
         FOCUS = 0,
         SHORT_BREAK,
-        LONG_BREAK
+        LONG_BREAK,
     };
 
     void initActionMap() noexcept;
-    void resetTimer() noexcept;
     void initPomodoro() noexcept;
     void initAudioEngine() noexcept;
     void initConfiguration() noexcept;
     void initKeybinds() noexcept;
     void initGui() noexcept;
+
+    void resetTimer() noexcept;
     void toggleTimer() noexcept;
+    void nextSession() noexcept;
+
     void updateCountdown() noexcept;
     void playSound() noexcept;
     void showNotification() noexcept;
-    int parse_duration(const std::string& input) noexcept;
+    int parse_duration(const std::string &input) noexcept;
     std::string secondsToFlexibleString(const int &totalSeconds) noexcept;
     void setupKeybindings(const QString &key, const QString &action) noexcept;
     void advanceState() noexcept;
     void updateStateLabel() noexcept;
     void updateRemainingLabel() noexcept;
-    std::string replacePlaceholder(std::string input, const std::string& key,
-                                   const std::string& value) noexcept;
+    std::string replacePlaceholder(std::string input, const std::string &key, const std::string &value) noexcept;
 
     QFont m_timer_font, m_state_font, m_remaining_font, m_paused_font;
-    bool m_timer_is_active { false },
-    m_hide_hour,
-    m_show_notif,
-    m_has_audio { false },
-    m_confirm_on_exit,
-    m_state_shown,
-    m_remaining_shown,
-    m_paused_shown;
+    bool m_timer_is_active{false}, m_hide_hour, m_show_notif, m_has_audio{false}, m_confirm_on_exit, m_state_shown,
+        m_remaining_shown, m_paused_shown;
 
-    int m_totalSeconds,
-    m_pomodoro_count = 0,
-    m_pomodoros_before_long_break = 4;
+    int m_totalSeconds, m_pomodoro_count = 0, m_pomodoros_before_long_break = 4;
 
-    BlinkingLabel *m_timer_label = new BlinkingLabel("00:00");
-    BlinkingLabel *m_state_label = new BlinkingLabel("Start");
+    BlinkingLabel *m_timer_label     = new BlinkingLabel("00:00");
+    BlinkingLabel *m_state_label     = new BlinkingLabel("Start");
     BlinkingLabel *m_remaining_label = new BlinkingLabel("Remaining");
-    BlinkingLabel *m_paused_label = new BlinkingLabel("PAUSED");
+    BlinkingLabel *m_paused_label    = new BlinkingLabel("PAUSED");
 
     QVBoxLayout *m_layout = new QVBoxLayout();
     std::string m_audio_file, m_notify_cmd;
@@ -93,6 +88,4 @@ private:
 
     ma_result m_audio_result;
     ma_engine m_audio_engine;
-
-
 };
